021cd046f490383dadb6bb59c92dcaa9b83d84b1
anjubora <anjuboura.2017@gmail.com>
2020-05-30 16:13:18 +0530

    code changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Angular
+          Learn React and Angular ,Vue
         </a>
       </header>
     </div>
