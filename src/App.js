3a0d4ff7df0c565595bf5231f19fbc995eaae976
anjubora <anjuboura.2017@gmail.com>
2020-05-30 18:35:34 +0530

    app.js updated

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Angular ,Vue
+          Welcome to test deployement on Elastic bean stalk by travis CI
         </a>
       </header>
     </div>
